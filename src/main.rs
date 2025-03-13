2a4112c47d6ec5483a682bc68d8957ddec401615
Springbock99 <daneelkent@gmail.com>
2025-03-13 12:38:27 +0100

    adding the rust hasher

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use sha3::{Digest, Keccak256};
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Welcome to RustHasher");
+    println!("Enter a smart contract function signer (e.g.., transfer(address, uint256)");
+
+    let mut input = String::new();
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read input");
+
+    let input = input.trim();
+    let mut hasher = Keccak256::new();
+    hasher.update(input);
+    let result = hasher.finalize();
+
+    let selector = &result[..4];
+    let hex_selector = hex::encode(selector);
+
+    println!("Function Selector (Keccak-256) :0x{}", hex_selector);
+
+
 }
